40a88de99c217f0de4157412369de8657e6cc5cf
dev.Rodrigo Niskier <niskier.rodrigo@gmail.com>
2026-08-10 15:16:20 -0300

    Use normalized RGM authentication form

--- config/urls.py
+++ config/urls.py
@@ -2,9 +2,18 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
+from core.forms import RGMAuthenticationForm
+
 urlpatterns = [
     path('django-admin/', admin.site.urls),
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path(
+        'login/',
+        auth_views.LoginView.as_view(
+            template_name='registration/login.html',
+            authentication_form=RGMAuthenticationForm,
+        ),
+        name='login',
+    ),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', include('core.urls')),
 ]
